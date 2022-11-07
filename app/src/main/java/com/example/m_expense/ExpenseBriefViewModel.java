9e36d29112e5edeff2914c9dbb4e2fd1fed935b9
Edward Nguyen <monokaijs@gmail.com>
2022-11-07 19:51:06 +0700

    Update list element style

--- app/src/main/java/com/example/m_expense/ExpenseBriefViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.m_expense;
-
-import androidx.lifecycle.ViewModel;
-
-public class ExpenseBriefViewModel extends ViewModel {
-  // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/m_expense/HomeFragment.java
+++ app/src/main/java/com/example/m_expense/HomeFragment.java
@@ -33,10 +33,7 @@ public class HomeFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-
-    return rootView;
+    return inflater.inflate(R.layout.fragment_home, container, false);
   }
 
   @Override
